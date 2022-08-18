40a92f16c8c3473bf9e4d80b4b0cda9e19c18616
Richard Poettler <richard.poettler@gmail.com>
2022-08-18 13:53:52 +0200

    Code formatting

--- masterthesis/unpack.py
+++ masterthesis/unpack.py
@@ -21,76 +21,94 @@ import re
 breakout = None
 
 # standard file format and desired years
-fileformat = 'incal-hourly_%Y0511T0000_%Y0521T0000.nc'
-years = ['2015','2016','2017']
+fileformat = "incal-hourly_%Y0511T0000_%Y0521T0000.nc"
+years = ["2015", "2016", "2017"]
 
-filepath = r'/media/iwbworkstation/My Passport/Dissertation/3_Hydrology/Muerz/basedata/INCA_DATA/free_Data/netcdf_files/INCA_2012-2017'
+filepath = r"/media/iwbworkstation/My Passport/Dissertation/3_Hydrology/Muerz/basedata/INCA_DATA/free_Data/netcdf_files/INCA_2012-2017"
 
 # name in INCA File, Destination path,output format, scaling factor
 extract_variables = [
-        ['RR','/media/iwbworkstation/My Passport/Dissertation/3_Hydrology/Muerz/basedata/INCA_DATA/free_Data/netcdf_files/RR','Muerz_RR_%Y%m%d_%H%M.tif','0.001'],
-        ['T2M','/media/iwbworkstation/My Passport/Dissertation/3_Hydrology/Muerz/basedata/INCA_DATA/free_Data/netcdf_files/TT','Muerz_TT_%Y%m%d_%H%M.tif','0.01'],
-        ['GL','/media/iwbworkstation/My Passport/Dissertation/3_Hydrology/Muerz/basedata/INCA_DATA/free_Data/netcdf_files/GL','Muerz_GL_%Y%m%d_%H%M.tif','0.01']
-        ]
+    [
+        "RR",
+        "/media/iwbworkstation/My Passport/Dissertation/3_Hydrology/Muerz/basedata/INCA_DATA/free_Data/netcdf_files/RR",
+        "Muerz_RR_%Y%m%d_%H%M.tif",
+        "0.001",
+    ],
+    [
+        "T2M",
+        "/media/iwbworkstation/My Passport/Dissertation/3_Hydrology/Muerz/basedata/INCA_DATA/free_Data/netcdf_files/TT",
+        "Muerz_TT_%Y%m%d_%H%M.tif",
+        "0.01",
+    ],
+    [
+        "GL",
+        "/media/iwbworkstation/My Passport/Dissertation/3_Hydrology/Muerz/basedata/INCA_DATA/free_Data/netcdf_files/GL",
+        "Muerz_GL_%Y%m%d_%H%M.tif",
+        "0.01",
+    ],
+]
 
 i = 0
 for subdir, dirs, files in os.walk(filepath):
-    # loops of files in folder AND SORTES them alphabetically 
+    # loops of files in folder AND SORTES them alphabetically
     # metadata is used from precipitation
     # find occurences of year %Y
-    years_ext = [m.start() for m in re.finditer('%Y', fileformat)]
-    years_ext = [int(years_ext[i] + i*2.) for i in range(0,len(years_ext))]
+    years_ext = [m.start() for m in re.finditer("%Y", fileformat)]
+    years_ext = [int(years_ext[i] + i * 2.0) for i in range(0, len(years_ext))]
     # file loop
     for file in sorted(files):
         # check if file contains our date
         # i know, this implementation could cause some problems at certain constellations
-        year_list = [file[start:start+4] for start in years_ext]
+        year_list = [file[start : start + 4] for start in years_ext]
         # check if any year is present in list
         if any(year in year_list for year in years):
             # process netcdf file
             # get time of netcdf file
-            net_path = subdir + '/' + file
+            net_path = subdir + "/" + file
             ds = nc.Dataset(net_path)
-            time = ds['time'][:]
-            time_array = num2date(ds['time'][:], ds['time'].units)
+            time = ds["time"][:]
+            time_array = num2date(ds["time"][:], ds["time"].units)
 
             ds = None
             # time loop
-            for t in range(0,len(time_array)):
+            for t in range(0, len(time_array)):
                 # variable loop
                 current_time = time_array[t].strftime(extract_variables[0][2])
-                
+
                 for var in extract_variables:
                     # read variable
                     ds = gdal.Open("NETCDF:{0}:{1}".format(net_path, var[0]))
                     # get respective raster band
-                    band = ds.GetRasterBand(t+1)
-                    # retrive data 
-                    data = band.ReadAsArray(0, 0, ds.RasterXSize, ds.RasterYSize).astype(np.float)
+                    band = ds.GetRasterBand(t + 1)
+                    # retrive data
+                    data = band.ReadAsArray(
+                        0, 0, ds.RasterXSize, ds.RasterYSize
+                    ).astype(np.float)
                     # scale parameters
                     data = data * float(var[3])
-                    
+
                     # create empty GTIFF
                     # build filepath
-                    tif_path = var[1] + '/' + current_time
+                    tif_path = var[1] + "/" + current_time
                     # create driver
-                    driver = gdal.GetDriverByName('GTiff')
+                    driver = gdal.GetDriverByName("GTiff")
                     # create ds out
-                    ds_out = driver.Create(tif_path, ds.RasterXSize,ds.RasterYSize,1,gdal.GDT_Float32)
+                    ds_out = driver.Create(
+                        tif_path, ds.RasterXSize, ds.RasterYSize, 1, gdal.GDT_Float32
+                    )
                     # get and set geotransform and projection
                     geotrans = ds.GetGeoTransform()
                     proj = ds.GetProjection()
                     ds_out.SetGeoTransform(geotrans)
-                    ds_out.SetProjection(proj)                    
-                    
+                    ds_out.SetProjection(proj)
+
                     # write data to raster
                     ds_out.GetRasterBand(1).WriteArray(data)
-                    
+
                     ds = None
                     ds_out = None
 
+            i += 1
 
-            i+=1
-        
         if i == breakout:
             break
