187814cdd0ed01132b1f3781aafc8964f5e22426
Richard Poettler <richard.poettler@gmail.com>
2023-10-21 10:06:32 +0200

    Copied scripts from postscripts

---
+++ masterthesis/extract_post_gauges.py
@@ -0,0 +1,445 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 22 09:22:17 2022
+
+@author: iwbworkstation
+"""
+
+import datetime
+import os
+import os.path as path
+from argparse import ArgumentParser
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pytz
+from osgeo import gdal
+
+gdal.UseExceptions()
+
+TEXTWITH_IN = 4.7747
+SIMULATION_START_DATE = datetime.datetime(2013, 1, 1, 0, 0)
+CALIBRATION_START_DATE = datetime.datetime(2014, 1, 1, 0, 0)
+OFFICIAL_END_DATE = datetime.datetime(2018, 1, 1, 0, 0)
+
+
+def nse(measured, simulated) -> float:
+    return (
+        1
+        - ((simulated - measured) ** 2).sum()
+        / ((measured - measured.mean()) ** 2).sum()
+    )
+
+
+def nce(measured, simulated) -> float:
+    return 1.0 - abs(measured - simulated).sum() / measured.sum()
+
+
+def kge(measured, simulated) -> float:
+    m1, m2 = np.mean(measured), np.mean(simulated)
+    r = np.sum((measured - m1) * (simulated - m2)) / (
+        np.sqrt(np.sum((measured - m1) ** 2)) * np.sqrt(np.sum((simulated - m2) ** 2))
+    )
+    beta = m2 / m1
+    gamma = (np.std(simulated) / m2) / (np.std(measured) / m1)
+    return 1 - np.sqrt((r - 1) ** 2 + (beta - 1) ** 2 + (gamma - 1) ** 2)
+
+
+def getRaster(filename, no_data=np.nan):
+    ds = gdal.Open(filename)
+    raster = ds.GetRasterBand(1).ReadAsArray()
+    no_data_value = ds.GetRasterBand(1).GetNoDataValue()
+    raster[raster == no_data_value] = no_data
+    return raster
+
+
+def stat_outmap_max(filenames):
+    result = np.zeros((len(filenames)))
+    for i, filename in enumerate(filenames):
+        raster = getRaster(filename)
+        result[i] = np.nanmax(raster)
+    return result
+
+
+def stat_outmap_sum(filenames):
+    result = np.zeros((len(filenames)))
+    for i, filename in enumerate(filenames):
+        raster = getRaster(filename)
+        result[i] = np.nansum(raster)
+    return result
+
+
+def stat_outmap_average(filenames):
+    result = np.zeros((len(filenames)))
+    for i, filename in enumerate(filenames):
+        raster = getRaster(filename)
+        result[i] = np.nanmean(raster)
+    return result
+
+
+def stat_outmaps(directory):
+    _, _, filenames = next(os.walk(directory))
+    filenames = sorted(filenames)
+    prefixed_files = {}
+    for filename in filenames:
+        prefix = filename[:3]
+        if prefix not in prefixed_files:
+            prefixed_files[prefix] = []
+        prefixed_files[prefix].append(path.join(directory, filename))
+
+    warumup_duration = CALIBRATION_START_DATE - SIMULATION_START_DATE
+    warm_up_hours = int(divmod(warumup_duration.total_seconds(), 3600)[0])
+
+    return {
+        "precipitation_sum": stat_outmap_sum(prefixed_files["pre"][warm_up_hours:]),
+        "precipitation_max": stat_outmap_max(prefixed_files["pre"][warm_up_hours:]),
+        "evaporation_average": stat_outmap_average(
+            prefixed_files["pot"][warm_up_hours:]
+        ),
+        "temperature_average": stat_outmap_average(
+            prefixed_files["tem"][warm_up_hours:]
+        ),
+    }
+
+
+def eval_peak_distance(meas_data, sim_data, hq1, window):
+    # find indices in meas data larger than hq1
+    indices = np.where(meas_data > hq1)
+
+    # create chuncks
+
+    chunks = []
+
+    chunk = []
+
+    for i in range(0, len(indices[0]) - 1):
+        # print(indices[0][i])
+        if len(chunk) == 0:
+            chunk.append(indices[0][i])
+        if abs(indices[0][i] - indices[0][i + 1]) < window:
+            chunk.append(indices[0][i + 1])
+        else:
+            chunks.append(chunk)
+            chunk = []
+
+    # reinit array blow up chuncks to measurement array
+    chuncks_blown = []
+
+    for chunk in chunks:
+        min_val = min(chunk) - window
+        max_val = max(chunk) + window
+
+        range_new = list(range(min_val, max_val))
+
+        chuncks_blown.append(range_new)
+
+    # compute distances
+
+    distances = []
+
+    for chunck in chuncks_blown:
+        val_meas_i = np.asarray(meas_data[chunck])
+        val_sim_i = np.asarray(sim_data[chunck])
+
+        max_scale_values = max(max(val_meas_i), max(val_sim_i))
+        min_scale_values = min(min(val_meas_i), min(val_sim_i))
+
+        # normalize
+        time_norm = np.linspace(0, 1, len(chunck))
+        val_meas_norm = (val_meas_i - min_scale_values) / (
+            max_scale_values - min_scale_values
+        )
+        val_sim_norm = (val_sim_i - min_scale_values) / (
+            max_scale_values - min_scale_values
+        )
+
+        # compute distance
+        x_time_sim = time_norm[np.where(val_sim_norm == max(val_sim_norm))[0][0]]
+        y_val_sim = val_sim_norm[np.where(val_sim_norm == max(val_sim_norm))[0][0]]
+        x_time_meas = time_norm[np.where(val_meas_norm == max(val_meas_norm))[0][0]]
+        y_val_meas = val_meas_norm[np.where(val_meas_norm == max(val_meas_norm))[0][0]]
+
+        dist = np.sqrt(
+            (x_time_meas - x_time_sim) ** 2.0 + (y_val_meas - y_val_sim) ** 2.0
+        )
+        distances.append(dist)
+
+    eval_metric = np.mean(np.asarray(distances))
+
+    return eval_metric
+
+
+def read_data(timezone, filepath, lower, upper):
+    # .timestamp() to convert to unix epoch
+    # convert back to timestamp -> datetime.datetime.fromtimestamp(t0)
+    time_list = []
+    value_list = []
+    with open(filepath, encoding="iso-8859-1") as f:
+        for line in f:
+            try:
+                line = line.strip()
+                line = line.split(";")
+                date = line[0].strip(" ")
+                value = line[1].strip(" ").replace(",", ".")
+
+                date_obj = datetime.datetime.strptime(date, "%d.%m.%Y %H:%M:%S")
+
+                time_localized = timezone.localize(date_obj)
+
+                time_unix = time_localized.timestamp()
+
+                if time_unix >= lower and time_unix <= upper:
+                    time_list.append(time_unix)
+
+                    if value.replace(".", "0").isdigit():
+                        value_list.append(float(value))
+                    else:
+                        value_list.append(np.nan)
+
+            except:
+                continue
+
+    value_list = np.asarray(value_list)
+    nans, x = np.isnan(value_list), lambda z: z.nonzero()[0]
+    value_list[nans] = np.interp(x(nans), x(~nans), value_list[~nans])
+
+    gauge_data = np.column_stack((time_list, value_list))
+
+    return gauge_data
+
+
+def read_sim(filepath, lower, eval_lower, timestep, higher):
+    # determine header
+    f = open(filepath, "r")
+    data = f.read()
+    data = data.split("\n")
+    header = int(data[1]) + 2
+    f.close()
+
+    discharge_all = np.genfromtxt(filepath, skip_header=header)
+
+    time_list = []
+    discharge_list = []
+
+    for i in range(0, len(discharge_all)):
+        timei = lower + i * timestep
+
+        if timei >= eval_lower and timei <= higher:
+            time_list.append(timei)
+            discharge_list.append(discharge_all[i, 1:])
+
+    time_list_np = np.asarray(time_list)
+    discharge_np = np.asarray(discharge_list)
+
+    return time_list_np, discharge_np
+
+
+def get_calibration_column_names(df):
+    result = []
+    for c in df.columns:
+        if c.startswith("calibrated_"):
+            result.append(c)
+    return result
+
+
+def compute_data_frame():
+    # gauges direcotry
+    list_gauges = {
+        "1": {
+            "name": "kapfenberg/Diemlach-Muerz QOW3082",
+            "path": r"/data/home/richi/master_thesis/model_MAR/gauges/Qow3082.csv",
+            "sCatch": 1,
+            "Index": 4,
+        },
+    }
+
+    timezone = pytz.timezone("Etc/GMT-1")
+
+    lower_obj = datetime.datetime.strptime("2013-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")
+    # lower localized
+    lower_localized = timezone.localize(lower_obj)
+    # lwoer timestamp
+    lower = lower_localized.timestamp()
+
+    eval_lower_obj = datetime.datetime.strptime(
+        "2014-01-01 00:00:00", "%Y-%m-%d %H:%M:%S"
+    )
+    # lower localized
+    eval_lower_localized = timezone.localize(eval_lower_obj)
+    # lwoer timestamp
+    eval_lower = eval_lower_localized.timestamp()
+
+    higher_obj = datetime.datetime.strptime("2017-12-28 00:00:00", "%Y-%m-%d %H:%M:%S")
+    # lower localized
+    higher_localized = timezone.localize(higher_obj)
+    # lwoer timestamp
+    higher = higher_localized.timestamp()
+
+    timestep = 3600
+
+    res_path = r"/data/home/richi/master_thesis/model_MAR/results_INCA/run.tss"
+    res_path2 = r"/data/home/richi/master_thesis/model_MAR/results_ERA5/run.tss"
+    res_path3 = r"/data/home/richi/master_thesis/model_MAR/results_ERA5_KGE/run.tss"
+
+    # read INCA simulation
+    time_sim, discharge_sim = read_sim(res_path, lower, eval_lower, timestep, higher)
+    time_sim2, discharge_sim2 = read_sim(res_path2, lower, eval_lower, timestep, higher)
+    time_sim3, discharge_sim3 = read_sim(res_path3, lower, eval_lower, timestep, higher)
+
+    gauge_path = list_gauges["1"]["path"]
+    gauge_index = int(list_gauges["1"]["Index"]) - 1
+    gauge_data = read_data(timezone, gauge_path, eval_lower, higher)
+
+    if (
+        not np.array_equal(time_sim, time_sim2)
+        or not np.array_equal(time_sim, time_sim3)
+        or not np.array_equal(time_sim, gauge_data[:, 0])
+    ):
+        raise ValueError("time columns must be equal")
+
+    date = gauge_data[:, 0]
+    to_datetime = np.vectorize(datetime.datetime.fromtimestamp)
+    datetimes = to_datetime(date)
+    measured = gauge_data[:, 1]
+
+    inca_stats = stat_outmaps(
+        "/data/home/richi/master_thesis/model_MAR/results_INCA/outmaps"
+    )
+    era5_stats = stat_outmaps(
+        "/data/home/richi/master_thesis/model_MAR/results_ERA5/outmaps"
+    )
+
+    return pd.DataFrame(
+        {
+            "date": datetimes,
+            "measured": measured,
+            "calibrated_inca_nse": discharge_sim[:, gauge_index],
+            "calibrated_era5_nse": discharge_sim2[:, gauge_index],
+            "calibrated_era5_kge": discharge_sim3[:, gauge_index],
+            "inca_precipitation_sum": inca_stats["precipitation_sum"][: len(datetimes)],
+            "inca_precipitation_max": inca_stats["precipitation_max"][: len(datetimes)],
+            "inca_evaporation_average": inca_stats["evaporation_average"][
+                : len(datetimes)
+            ],
+            "inca_temperature_average": inca_stats["temperature_average"][
+                : len(datetimes)
+            ],
+            "era5_precipitation_sum": era5_stats["precipitation_sum"][: len(datetimes)],
+            "era5_precipitation_max": era5_stats["precipitation_max"][: len(datetimes)],
+            "era5_evaporation_average": era5_stats["evaporation_average"][
+                : len(datetimes)
+            ],
+            "era5_temperature_average": era5_stats["temperature_average"][
+                : len(datetimes)
+            ],
+        }
+    )
+
+
+def plot_df(df):
+    column_names = get_calibration_column_names(df)
+    fig, axs = plt.subplots(len(column_names))
+    for index, name in enumerate(column_names):
+        axs[index].plot(df["date"], df["measured"], "k--", label="measured")
+        axs[index].plot(df["date"], df[name], "b", label="simulated")
+        axs[index].grid()
+        axs[index].legend()
+    plt.show()
+    plt.close(fig)
+
+
+def plotWeather(df):
+    fig, axs = plt.subplots(3)
+    axs[0].set_title("Precipitation Sum")
+    axs[0].plot(df["date"], df["inca_precipitation_sum"], "k--", label="inca")
+    axs[0].plot(df["date"], df["era5_precipitation_sum"], "b", label="era5")
+    axs[0].grid()
+    axs[0].legend()
+    axs[1].set_title("Temperature Average")
+    axs[1].plot(df["date"], df["inca_temperature_average"], "k--", label="inca")
+    axs[1].plot(df["date"], df["era5_temperature_average"], "b", label="era5")
+    axs[1].grid()
+    axs[1].legend()
+    axs[2].set_title("Evaporation Average")
+    axs[2].plot(df["date"], df["inca_evaporation_average"], "k--", label="inca")
+    axs[2].plot(df["date"], df["era5_evaporation_average"], "b", label="era5")
+    axs[2].grid()
+    axs[2].legend()
+    plt.show()
+    plt.close(fig)
+
+
+def parseDate(s):
+    return datetime.datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
+
+
+def export_pgf(df, folder):
+    plt.rcParams.update(
+        {
+            "pgf.preamble": r"\usepackage{siunitx}",
+            # "pgf.texsystem": "pdflatex",
+            "font.family": "serif",
+            # "text.usetex": True,
+            "pgf.rcfonts": False,
+        }
+    )
+    for name in get_calibration_column_names(df):
+        fig, axs = plt.subplots()
+        fig.set_size_inches(w=TEXTWITH_IN, h=TEXTWITH_IN)
+        plt.xlabel("Date")
+        axs.set_xlim(left=df["date"].iloc[0], right=OFFICIAL_END_DATE)
+        plt.xticks(rotation=45, ha="right")
+        plt.ylabel("Discharge [\\si{\\cubic\\meter\\per\\second}]")
+        axs.plot(df["date"], df["measured"], label="Measured")
+        axs.plot(df["date"], df[name], label="Simulated")
+        axs.grid()
+        axs.legend()
+        fig.tight_layout()
+        fig.savefig(path.join(folder, f"{name}.pgf"), backend="pgf")
+        plt.close(fig)
+
+
+def print_stats(df):
+    for name in get_calibration_column_names(df):
+        print(f"= {name}")
+        print(f"NSE = {nse(df['measured'], df[name])}")
+        print(f"NCE = {nce(df['measured'], df[name])}")
+        print(f"KGE = {kge(df['measured'], df[name])}")
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-c", "--csv", metavar="file", help="write csv data to file")
+    parser.add_argument("-p", "--plot", action="store_true", help="show plot")
+    parser.add_argument("-s", "--stats", action="store_true", help="print stats")
+    parser.add_argument(
+        "-l", "--latex", metavar="folder", help="write pgf files to folder"
+    )
+    parser.add_argument("-i", "--input", metavar="file", help="use data from csv file")
+    parser.add_argument(
+        "-w", "--weather", action="store_true", help="plot weather data"
+    )
+    args = parser.parse_args()
+
+    df = (
+        compute_data_frame()
+        if not args.input
+        else pd.read_csv(args.input, parse_dates=["date"], date_parser=parseDate)
+    )
+
+    if args.plot:
+        plot_df(df)
+    if args.latex:
+        export_pgf(df, args.latex)
+    if args.csv:
+        df.to_csv(args.csv, index=False)
+    if args.stats:
+        print_stats(df)
+    if args.weather:
+        plotWeather(df)
+
+
+if __name__ == "__main__":
+    main()
---
+++ masterthesis/staticmap_plot.py
@@ -0,0 +1,326 @@
+#!/usr/bin/env python3
+
+import matplotlib.colors as colors
+import matplotlib.figure as figure
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.ma as ma
+import numpy.typing as typing
+
+import extract_post_gauges
+
+CATCHMENT_FILE = (
+    "/data/home/richi/master_thesis/model_MAR/staticmaps/wflow_subcatch.map"
+)
+DEM_FILE = "/data/home/richi/master_thesis/model_MAR/staticmaps/wflow_dem.map"
+RIVER_FILE = "/data/home/richi/master_thesis/model_MAR/staticmaps/wflow_river.map"
+GAUGE_FILE = "/data/home/richi/master_thesis/model_MAR/staticmaps/wflow_gauges.map"
+STREAMORDER_FILE = (
+    "/data/home/richi/master_thesis/model_MAR/staticmaps/wflow_streamorder.map"
+)
+LDD_FILE = "/data/home/richi/master_thesis/model_MAR/staticmaps/wflow_ldd.map"
+LANDUSE_FILE = "/data/home/richi/master_thesis/model_MAR/staticmaps/wflow_landuse.map"
+
+DEM_PGF = "/data/home/richi/master_thesis/thesis/images/wflow_dem.pgf"
+RIVER_PGF = "/data/home/richi/master_thesis/thesis/images/wflow_river.pgf"
+STREAMORDER_PGF = "/data/home/richi/master_thesis/thesis/images/wflow_streamorder.pgf"
+LDD_PGF = "/data/home/richi/master_thesis/thesis/images/wflow_ldd.pgf"
+LANDUSE_PGF = "/data/home/richi/master_thesis/thesis/images/wflow_landuse.pgf"
+LANDUSE_LEVEL_3_PGF = (
+    "/data/home/richi/master_thesis/thesis/images/wflow_landuse_level_3.pgf"
+)
+
+WIDTH_IN = extract_post_gauges.TEXTWITH_IN
+HEIGHT_IN = WIDTH_IN
+
+LANDUSE_CODES = {
+    1: {
+        "name": "Artificial Surfaces",
+        1: {
+            "name": "Urban fabric",
+            1: {"name": "Continuous urban fabric"},
+            2: {"name": "Discontinuous urban fabric"},
+        },
+        2: {
+            "name": "Industrial, commercial and transport units",
+            1: {"name": "Industrial or commercial units"},
+            2: {"name": "Road and rail networks and associated land"},
+            3: {"name": "Port areas"},
+            4: {"name": "Airports"},
+        },
+        3: {
+            "name": "Mine, dump and construction sites",
+            1: {"name": "Mineral extraction sites"},
+            2: {"name": "Dump sites"},
+            3: {"name": "Construction sites"},
+        },
+        4: {
+            "name": "Artificial, non-agricultural vegetated areas",
+            1: {"name": "Green urban areas"},
+            2: {"name": "Sport and leisure facilities"},
+        },
+    },
+    2: {
+        "name": "Agricultural Areas",
+        1: {
+            "name": "Arable land",
+            1: {"name": "Non-irrigated arable land"},
+            2: {"name": "Permanently irrigated land"},
+            3: {"name": "Rice fields"},
+        },
+        2: {
+            "name": "Permanent crops",
+            1: {"name": "Vineyards"},
+            2: {"name": "Fruit trees and berry plantations"},
+            3: {"name": "Olive groves"},
+        },
+        3: {
+            "name": "Pastures",
+            1: {"name": "Pastures"},
+        },
+        4: {
+            "name": " Heterogeneous agricultural areas",
+            1: {"name": "Annual crops associated with permanent crops"},
+            2: {"name": "Complex cultivation patterns"},
+            3: {
+                "name": "Land principally occupied by agriculture, with significant areas of natural vegetation"
+            },
+            4: {"name": "Agro-forestry areas"},
+        },
+    },
+    3: {
+        "name": "Forest and Seminatural Areas",
+        1: {
+            "name": "Forests",
+            1: {"name": "Broad-leaved forest"},
+            2: {"name": "Coniferous forest"},
+            3: {"name": "Mixed forest"},
+        },
+        2: {
+            "name": "Scrub and/or herbaceous associations",
+            1: {"name": "Natural grassland"},
+            2: {"name": "Moors and heathland"},
+            3: {"name": "Sclerophyllous vegetation"},
+            4: {"name": "Transitional woodland-scrub"},
+        },
+        3: {
+            "name": "Open spaces with little or no vegetation",
+            1: {"name": "Beaches, dunes, sands"},
+            2: {"name": "Bare rocks"},
+            3: {"name": "Sparsely vegetated areas"},
+            4: {"name": "Burnt areas"},
+            5: {"name": "Glaciers and perpetual snow"},
+        },
+    },
+    4: {
+        "name": "Wetlands",
+        1: {
+            "name": "Inland wetlands",
+            1: {"name": "Inland marshes"},
+            2: {"name": "Peat bogs"},
+        },
+        2: {
+            "name": "Marine wetlands",
+            1: {"name": "Salt marshes"},
+            2: {"name": "Salines"},
+            3: {"name": "Intertidal flats"},
+        },
+    },
+    5: {
+        "name": "Water Bodies",
+        1: {
+            "name": "Inland waters",
+            1: {"name": "Water courses"},
+            2: {"name": "Water bodies"},
+        },
+        2: {
+            "name": "Marine waters",
+            1: {"name": "Coastal lagoons"},
+            2: {"name": "Estuaries"},
+            3: {"name": "Sea and ocean"},
+        },
+    },
+}
+
+
+def savefig(fig: figure.Figure, filename: str) -> None:
+    fig.tight_layout()
+    if filename.endswith(".pgf"):
+        fig.savefig(filename, backend="pgf")
+    else:
+        fig.savefig(filename)
+
+
+def getCatchmentMask(catchment_file: str) -> typing.NDArray:
+    catchment_mask = extract_post_gauges.getRaster(catchment_file)
+    catchment_mask[catchment_mask == 1] = 0
+    catchment_mask[catchment_mask == np.nan] = 1
+    return catchment_mask
+
+
+def generateDem(dem_file: str, catchment_file: str) -> typing.NDArray:
+    return ma.masked_array(
+        extract_post_gauges.getRaster(dem_file), mask=getCatchmentMask(catchment_file)
+    )
+
+
+def writeDem(dem_file: str, catchment_file: str, outfile: str) -> None:
+    fig, ax = plt.subplots()
+    fig.set_size_inches(w=WIDTH_IN, h=HEIGHT_IN)
+    ax.tick_params(bottom=False, labelbottom=False, left=False, labelleft=False)
+
+    dem = ma.masked_array(
+        extract_post_gauges.getRaster(dem_file), mask=getCatchmentMask(catchment_file)
+    )
+    dem_ax = ax.imshow(dem)
+    label = "Elevation [\\si{\\meter}]" if outfile.endswith(".pgf") else "Elevation [m]"
+    fig.colorbar(
+        dem_ax,
+        ax=ax,
+        label=label,
+        orientation="horizontal",
+    )
+
+    savefig(fig, outfile)
+    plt.close(fig)
+
+
+def writeRiverAndGauges(
+    dem_file: str, catchment_file: str, river_file: str, gauges_file: str, outfile: str
+) -> None:
+    river = extract_post_gauges.getRaster(river_file, no_data=-1)
+    river_mask = np.zeros(river.shape)
+    river_mask[river == -1] = 1
+    river_masked = ma.masked_array(river, mask=river_mask)
+
+    gauges = extract_post_gauges.getRaster(gauges_file, no_data=-1)
+    gauge_mask = np.zeros(gauges.shape)
+    gauge_mask[gauges == -1] = 1
+    gauges_masked = ma.masked_array(gauges, mask=gauge_mask)
+
+    fig, ax = plt.subplots()
+    fig.set_size_inches(w=WIDTH_IN, h=HEIGHT_IN)
+    ax.tick_params(bottom=False, labelbottom=False, left=False, labelleft=False)
+
+    dem_ax = ax.imshow(generateDem(dem_file, catchment_file), alpha=0.6)
+    label = "Elevation [\\si{\\meter}]" if outfile.endswith(".pgf") else "Elevation [m]"
+    fig.colorbar(dem_ax, ax=ax, label=label, orientation="horizontal")
+    river_map = colors.ListedColormap(["blue"])
+    ax.imshow(river_masked, cmap=river_map, interpolation="none")
+    gauge_colors = ["white" for _ in range(9)]
+    gauge_colors[2] = "red"
+    gauge_map = colors.ListedColormap(gauge_colors)
+    ax.imshow(gauges_masked, cmap=gauge_map, interpolation="none")
+
+    savefig(fig, outfile)
+    plt.close(fig)
+
+
+def writeStreamOrder(catchment_file: str, streamorder_file: str, outfile: str) -> None:
+    streamorder = ma.masked_array(
+        extract_post_gauges.getRaster(streamorder_file),
+        mask=getCatchmentMask(catchment_file),
+    )
+
+    fig, ax = plt.subplots()
+    fig.set_size_inches(w=WIDTH_IN, h=HEIGHT_IN)
+    ax.tick_params(bottom=False, labelbottom=False, left=False, labelleft=False)
+
+    stream_ax = ax.imshow(streamorder, cmap="plasma", interpolation="none")
+    cbar = fig.colorbar(stream_ax, ax=ax, label="Streamorder", orientation="horizontal")
+    ticks = range(int(np.nanmin(streamorder)), int(np.nanmax(streamorder)) + 1)
+    cbar.set_ticks(
+        ticks=ticks,
+        labels=[str(i) for i in ticks],
+    )
+
+    savefig(fig, outfile)
+    plt.close(fig)
+
+
+def writeLdd(catchment_file: str, ldd_file: str, outfile: str) -> None:
+    ldd = ma.masked_array(
+        extract_post_gauges.getRaster(ldd_file, -1),
+        mask=getCatchmentMask(catchment_file),
+    )
+
+    fig, ax = plt.subplots()
+    fig.set_size_inches(w=WIDTH_IN, h=HEIGHT_IN)
+    ax.tick_params(bottom=False, labelbottom=False, left=False, labelleft=False)
+
+    ldd_ax = ax.imshow(ldd, cmap="viridis", interpolation="none")
+    cbar = fig.colorbar(ldd_ax, ax=ax, label="Direction", orientation="horizontal")
+    ticks = range(int(np.nanmin(ldd)), int(np.nanmax(ldd)) + 1)
+    cbar.set_ticks(
+        ticks=ticks,
+        labels=[str(i) for i in ticks],
+    )
+
+    savefig(fig, outfile)
+    plt.close(fig)
+
+
+def writeLandUse(
+    catchment_file: str, landuse_file: str, outfile: str, outfile_level_3: str = ""
+) -> None:
+    landuse = ma.masked_array(
+        extract_post_gauges.getRaster(landuse_file, -1),
+        mask=getCatchmentMask(catchment_file),
+    )
+
+    fig, ax = plt.subplots()
+    fig.set_size_inches(w=WIDTH_IN, h=HEIGHT_IN)
+    ax.tick_params(bottom=False, labelbottom=False, left=False, labelleft=False)
+
+    landuse_ax = ax.imshow(landuse, cmap="viridis", interpolation="none")
+    fig.colorbar(landuse_ax, ax=ax, label="Landuse ID", orientation="horizontal")
+
+    savefig(fig, outfile)
+    plt.close(fig)
+
+    if outfile_level_3:
+        level3 = landuse // 100
+        level3_colors = {
+            1: "black",
+            2: "yellow",
+            3: "green",
+            4: "mediumaquamarine",
+            5: "blue",
+        }
+        used_ids = np.unique(level3.compressed())
+        level3_map = colors.ListedColormap([level3_colors[i] for i in used_ids])
+        patches = [
+            mpatches.Patch(color=level3_colors[i], label=LANDUSE_CODES[i]["name"])
+            for i in used_ids
+        ]
+
+        fig, ax = plt.subplots()
+        fig.set_size_inches(w=WIDTH_IN, h=HEIGHT_IN)
+        ax.tick_params(bottom=False, labelbottom=False, left=False, labelleft=False)
+
+        ax.imshow(level3, cmap=level3_map, interpolation="none")
+        fig.legend(handles=patches, loc="lower left")
+
+        savefig(fig, outfile_level_3)
+        plt.close(fig)
+
+
+def main() -> None:
+    plt.rcParams.update(
+        {
+            "pgf.preamble": r"\usepackage{siunitx}",
+            "font.family": "serif",
+            "pgf.rcfonts": False,
+        }
+    )
+
+    writeDem(DEM_FILE, CATCHMENT_FILE, DEM_PGF)
+    writeRiverAndGauges(DEM_FILE, CATCHMENT_FILE, RIVER_FILE, GAUGE_FILE, RIVER_PGF)
+    writeStreamOrder(CATCHMENT_FILE, STREAMORDER_FILE, STREAMORDER_PGF)
+    writeLdd(CATCHMENT_FILE, LDD_FILE, LDD_PGF)
+    writeLandUse(CATCHMENT_FILE, LANDUSE_FILE, LANDUSE_PGF, LANDUSE_LEVEL_3_PGF)
+
+
+if __name__ == "__main__":
+    main()
